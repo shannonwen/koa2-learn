9a8295bac67e35fd37825fafac3ae1c07d858c03
shannonwen <wenlongxiang@live.com>
2019-05-07 22:56:49 +0800

    redis的使用

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const users = require('./routes/users')
 const mongoose = require('mongoose')
 const dbConfig = require('./dbs/config')
 
+const session = require('koa-generic-session')
+const Redis = require('koa-redis')
+
+const pv = require('./middleware/koa-pv')
+
 // error handler
 onerror(app)
 
@@ -22,6 +27,7 @@ app.use(bodyparser({
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
+app.use(pv())
 
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
@@ -43,6 +49,32 @@ app.use(users.routes(), users.allowedMethods())
 mongoose.connect(dbConfig.dbs,{
   useNewUrlParser: true
 })
+
+//session处理
+app.keys = ['keys', 'keyskeys']
+// app.use(session({
+//   key: 'wlx',
+//   prefix: 'mtpr',
+//   store: new Redis()
+// }))
+
+
+app.use( async ( ctx ) => {
+  // 设置session
+  if ( ctx.url === '/set' ) {
+	ctx.session = {
+	  user_id: Math.random().toString(36).substr(2),
+	  count: 0
+	}
+	ctx.body = ctx.session
+  } else if ( ctx.url === '/' ) {
+ 
+	// 读取session信息
+	ctx.session.count = ctx.session.count + 1
+	ctx.body = ctx.session
+  }
+})
+
 
 // error-handling
 app.on('error', (err, ctx) => {
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ const router = require('koa-router')()
 //引入模型
 const Person = require('../dbs/models/person')
 
+const Redis = require('koa-redis')
+
+const Store = new Redis().client
+
 router.prefix('/users')
 
 router.get('/', function (ctx, next) {
@@ -64,6 +68,13 @@ router.post('/removePerson', async function(ctx){
   }).remove()
   ctx.body = {
     result
+  }
+})
+//redis
+router.get('/fix', async function(ctx){
+  const st = await Store.hset('fix', 'name', Math.random())
+  ctx.body = {
+    code: 0
   }
 })
 
